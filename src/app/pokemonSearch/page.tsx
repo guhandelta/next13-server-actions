fe3675c07c4b11299ea44e5dbb505ef224e1a22a
guhandelta <guhan.delta@gmail.com>
2023-05-13 12:39:10 -0400

    Added some more docs on fetch() & Next13

--- src/app/pokemonSearch/page.tsx
+++ src/app/pokemonSearch/page.tsx
@@ -6,6 +6,15 @@ export default function PokemonSearch() {
         "use server";
         console.log("Searching for...", search);
         
+        /* This fetch() is cached, as NextJS would overwrite this fetch with a cached fetch, so the UI
+        would make the call to this API endpoint only once
+        This server action does not page revalidate => refresh the page, as there is no mutation on the 
+        server side and nothing is changing on the server. All that is done here is returning the data
+        out of this fn() as a normal fn() does
+
+        GitHub - Theo Browne's - Zact, Zod Server ACTions =allows=> validated typesafe API's using this
+        mechanism
+        */
         const pokemonRes = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
 
         const pokeData = await pokemonRes.json(); 
